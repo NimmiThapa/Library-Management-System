b610ef780ea943de3fec727492979c12228a9486
Nimmi Thapa <tnimmi10@gmail.com>
2025-07-22 13:28:42 +0530

    Initial commit - JavaApplication6 project

---
+++ README.md.txt
@@ -0,0 +1,23 @@
+# Library Management System
+
+This is a Java-based Library Management System developed using NetBeans IDE.  
+It manages books, students, and transactions such as issuing and returning books.
+
+## 🔧 Technologies Used:
+- Java (Swing for GUI)
+- NetBeans IDE
+- MySQL Database
+
+## 📌 Features:
+- Add, update, delete books and students
+- Issue and return books
+- View transaction history
+
+## 🚀 How to Run:
+1. Open the project in NetBeans.
+2. Import `database.sql` into your MySQL server.
+3. Configure DB connection in your code (if required).
+4. Run the project.
+
+## 👩‍💻 Author:
+Nimmi Thapa
---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author hp
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Form obj=new Form();
+        //AddBook obj = new AddBook();
+        //BookIssue obj = new BookIssue();
+        //CreateUser obj=new CreateUser();
+        //Update_Book_Record obj = new Update_Book_Record();
+        //Update_stu_record obj=new Update_stu_record ();
+        //Afterlogin obj = new Afterlogin();
+        //Book_Return obj=new Book_Return();
+        //Delete_Student obj=new Delete_Student();
+        //Delete_Book obj=new Delete_Book();
+        //Afterlogin obj = new Afterlogin();
+        //Forgot_Password obj=new Forgot_Password();
+        obj.show();
+    }
+    
+}
